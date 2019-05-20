bc610a85184606812b1d4b7650dce759fe220ea4
meperla <32370186+meperla@users.noreply.github.com>
2019-05-20 09:58:06 -0700

    Add files via upload

--- Mirroring.ino
+++ Mirroring.ino
@@ -12,23 +12,33 @@
 
 //#define BRIDGE "Lab Green"
 //#define BRIDGE "Lab Blue"
-//#define BRIDGE "Lab Red"
-#define BRIDGE "Sieg Master"
+#define BRIDGE "Lab Red"
+//#define BRIDGE "Sieg Master"
 
-const int button1Pin = 12;
-const int button2Pin = 13;
-const int button3Pin = 15;
+const int button1Pin = 14;
+const int button2Pin = 12;
+const int button3Pin = 13;
+const int button4Pin = 15;
+
+int countButton1 = 0;
+int countButton2 = 0;
+int countButton3 = 0;
+int countButton4 = 0;
 
 int button1State = 0;
 int button2State = 0;
 int button3State = 0;
+int button4State = 0;
+
+int emotion = 1;
+int count = countButton1;
 
 String ip; //Hue Bridge IP Address
 String api_token; //Hue Bridge Authentication api_token
 
 
 void changeGroup(byte groupNum, byte transitiontime, String parameter, String newValue, String parameter2 = "",
-                 String newValue2 = "", String parameter3 = "", String newValue3 = "") {
+                 String newValue2 = "", String parameter3 = "", String newValue3 = "",String parameter4 = "", String newValue4 = "") {
 
   String req_string = "http://" + ip + "/api/" + api_token + "/groups/" + groupNum + "/action";
   HTTPClient http;
@@ -37,7 +47,8 @@ void changeGroup(byte groupNum, byte transitiontime, String parameter, String ne
   String put_string = "{\"" + parameter + "\":" + newValue + ", \"transitiontime\": " +
                       transitiontime;
   if (!parameter2.equals("")) put_string += + ", \"" + parameter2 + "\": " + newValue2;
-  if (!parameter3.equals("")) put_string += ", \"" + parameter3 + "\" : " + newValue3;
+  if (!parameter3.equals("")) put_string += + ", \"" + parameter3 + "\": " + newValue3;
+  if (!parameter4.equals("")) put_string += ", \"" + parameter4 + "\" : " + newValue4;
   put_string +=  + "}";
 
   Serial.println("Attempting PUT: " + put_string + " for GROUP: " + String(groupNum));
@@ -122,8 +133,11 @@ void setup() {
   pinMode(button1Pin, INPUT);
   pinMode(button2Pin, INPUT);
   pinMode(button3Pin, INPUT);
+  pinMode(button4Pin, INPUT);
 
   setup_wifi();
+
+  changeGroup(4, 3, "on", "true", "hue", "0", "bri", "100", "sat", "0");
  
 }
 
@@ -132,6 +146,22 @@ void loop() {
   button1();
   button2();
   button3();
+  button4();
+
+  getEmotion();
+  visualize();
+
+//  Serial.print("button 1 = ");
+//  Serial.println(countButton1);
+//
+//  Serial.print("button 2 = ");
+//  Serial.println(countButton2);
+//
+//  Serial.print("button 3 = ");
+//  Serial.println(countButton3);
+//
+//  Serial.print("button 4 = ");
+//  Serial.println(countButton4);
 }
 
 //Happy _ yellow & orange
@@ -139,8 +169,7 @@ void button1() {
   button1State = digitalRead(button1Pin);
   //Serial.println(button1State);
   if (button1State == HIGH) {
-    changeGroup(3, 3, "on", "true", "hue", "60000", "bri", "150");
-    changeGroup(4, 3, "on", "true", "hue", "50000", "bri", "150");
+    countButton1 += 1;
     delay(500);
   } else if(button1State == LOW) {
     
@@ -152,8 +181,7 @@ void button2() {
   button2State = digitalRead(button2Pin);
   //Serial.println(button2State);
   if (button2State == HIGH) {
-    changeGroup(3, 3, "on", "true", "hue", "40000", "bri", "150");
-    changeGroup(4, 3, "on", "true", "hue", "30000", "bri", "150");
+    countButton2 += 1;
     delay(500);
   } else if(button2State == LOW) {
     
@@ -165,17 +193,62 @@ void button3() {
   button3State = digitalRead(button3Pin);
   //Serial.println(button3State);
   if (button3State == HIGH) {
-    changeGroup(3, 3, "on", "true", "hue", "20000", "bri", "150");
-    changeGroup(4, 3, "on", "true", "hue", "5000", "bri", "150");
+    countButton3 += 1;
     delay(500);
   } else if(button3State == LOW) {
     
   }
 }
 
-//only for blue hub
-//group id 3 is bulbs 1 & 3
-//group id 4 is bulbs 2 & 4
+//Mad_ Red_Orange
+void button4() {
+  button4State = digitalRead(button4Pin);
+  //Serial.println(button4State);
+  if (button4State == HIGH) {
+    countButton4 += 1;
+    delay(500);
+  } else if(button4State == LOW) {
+    
+  }
+}
+
+void getEmotion() {
+  if(count < countButton2){
+    count = countButton2;
+    emotion = 2;
+  }
+  if(count < countButton3){
+    count = countButton3;
+    emotion = 3;
+  }
+  if(count < countButton4){
+    count = countButton4;
+    emotion = 4;
+  }  
+  Serial.print(count);
+  Serial.print(" presses for button: ");
+  Serial.println(emotion);
+}
+
+void visualize () {
+  if(emotion == 1){ //Happy
+    changeGroup(4, 3, "on", "true", "hue", "50000", "bri", "150", "sat", "150");
+    changeGroup(5, 3, "on", "true", "hue", "5000", "bri", "150", "sat", "150");
+  }
+  if(emotion == 2){ //Okay
+    changeGroup(4, 3, "on", "true", "hue", "40000", "bri", "150", "sat", "150");
+    changeGroup(5, 3, "on", "true", "hue", "20000", "bri", "150", "sat", "150");
+  }
+  if(emotion == 3){ //Sad
+    changeGroup(4, 3, "on", "true", "hue", "10000", "bri", "150", "sat", "150");
+    changeGroup(5, 3, "on", "true", "hue", "50000", "bri", "150", "sat", "150");
+  }
+ if(emotion == 4){ //Mad
+    changeGroup(4, 3, "on", "true", "hue", "30000", "bri", "150", "sat", "150");
+    changeGroup(5, 3, "on", "true", "hue", "50000", "bri", "150", "sat", "150");
+  }
+}
+
 
 
 
